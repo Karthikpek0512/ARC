de66d3f2cc5e977f0f4dc7a90dbea1955b161e52
Karthikpek0512 <94725155+Karthikpek0512@users.noreply.github.com>
2021-11-21 15:57:24 +0000

    Update manual_solve.py

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -10,15 +10,14 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
-    return x
+#def solve_0dfd9992(x):
+#    return x
 
-def solve_b2862040(x):
-    return x
+#def solve_3aa6fb7a(x):
+#    return x
 
-def solve_05269061(x):
+def solve_253bf280(x):
     return x
-
 
 def main():
     # Find all the functions defined in this file whose names are
